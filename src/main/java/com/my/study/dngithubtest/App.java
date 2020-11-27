a9724a166f798c1281d2409bfae011cf1dfdbeaa
2947039658 <2947039658@qq.com>
2020-11-27 15:40:57 +0800

    测试提交github

---
+++ src/main/java/com/my/study/dngithubtest/App.java
@@ -0,0 +1,13 @@
+package com.my.study.dngithubtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
